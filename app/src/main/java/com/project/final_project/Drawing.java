c95406886764e1c40e6602bc57d2cdda6cddc730
VictorVilo <githinjivictor9@gmail.com>
2020-02-19 10:37:43 +0300

    Added activities

--- app/src/main/java/com/project/final_project/Drawing.java
+++ app/src/main/java/com/project/final_project/Drawing.java
@@ -2,7 +2,9 @@ package com.project.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -15,5 +17,10 @@ public class Drawing extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_drawing);
+    }
+
+    public void startDrawing(View view) {
+        Intent intent = new Intent(Drawing.this, ActivityDraw.class);
+        startActivity(intent);
     }
 }
