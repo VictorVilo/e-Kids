cc1adb1631470a66e0587b819e47f56cecb43a5e
VictorVilo <githinjivictor9@gmail.com>
2020-02-20 13:11:44 +0300

    Added activities

--- app/src/main/java/com/project/final_project/Pattern.java
+++ app/src/main/java/com/project/final_project/Pattern.java
@@ -2,7 +2,9 @@ package com.project.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -15,5 +17,10 @@ public class Pattern extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pattern);
+    }
+
+    public void canvasClick(View view) {
+        Intent intent = new Intent(Pattern.this, EasyPaint.class);
+        startActivity(intent);
     }
 }
